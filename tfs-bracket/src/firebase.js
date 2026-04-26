b9c662fa87277c710ff4021a0e62712ce00c5468
frpanico96 <frpanico96@outlook.it>
2026-04-27 01:21:15 +0200

    [TFS] Bracket - First Commit

---
+++ tfs-bracket/src/firebase.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, query, orderBy, where, onSnapshot, serverTimestamp } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => signInWithPopup(auth, googleProvider);
+export const logOut = () => signOut(auth);
+
+export const tournamentsRef = collection(db, "tournaments");
+export const usersRef = collection(db, "users");
+
+export {
+  addDoc,
+  getDocs,
+  doc,
+  updateDoc,
+  query,
+  orderBy,
+  where,
+  onSnapshot,
+  serverTimestamp,
+};
